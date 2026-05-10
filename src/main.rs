67810cead177bb62914ad7cdd1af7f823f8fec57
void2610 <contact@void2610.dev>
2026-05-11 00:26:52 +0900

    feat: impl basic arg system

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,49 @@
+use clap::{Args, Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(version, about)]
+struct Cli {
+    #[command(subcommand)]
+    command: Command,
+}
+
+#[derive(Subcommand)]
+enum Command {
+    // Init,
+    // Health,
+    // Doctor,
+    /// コマンド一覧を取得する
+    Commands,
+    /// コマンドを実行する
+    Exec(ExecArgs),
+    // Logs,
+    // State,
+    // Scenarios,
+    // Run,
+}
+
+#[derive(Args)]
+struct ExecArgs {
+    /// 実行対象コマンドのパス
+    path: String,
+    /// コマンドに渡すパラメータ
+    params: Option<String>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    match cli.command {
+        Command::Commands => {
+            println!("コマンド一覧");
+        }
+        Command::Exec(args) => match args.params {
+            Some(p) => {
+                println!("{} にパラメータ{}を渡して実行しました", args.path, p);
+            }
+            None => {
+                println!("{} を実行しました", args.path);
+            }
+        },
+    }
 }
